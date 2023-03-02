ac7a2e87d940b689a12d99050a588f280b0ff952
montehurd <mhurd@wikimedia.org>
2023-03-02 13:42:25 -0800

    clear out any previous notes

--- extract-notes.sh
+++ extract-notes.sh
@@ -4,11 +4,16 @@
 cp "$HOME/Library/Group Containers/group.com.apple.notes/NoteStore.sqlite" "./"
 # make a folder to hold extracted notes
 mkdir -p "notes" 
-# extract each note to a file in notes folder
-sqlite3 NoteStore.sqlite "SELECT writefile('./notes/note-' || Z_PK || '.txt.gz', ZDATA) FROM ZICNOTEDATA" 
-# unzip each note - note blobs are stored in gz format
 cd "notes"
+# remove any previous extracted notes
+rm *.gz
+rm *.txt
+# extract each note to a file in notes folder
+sqlite3 ../NoteStore.sqlite "SELECT writefile('./note-' || Z_PK || '.txt.gz', ZDATA) FROM ZICNOTEDATA" 
+# unzip each note - note blobs are stored in gz format
 gunzip *.gz
+# remove any dangling gz files
+rm *.gz
 # remove copy of NoteStore
 cd -
 rm NoteStore*

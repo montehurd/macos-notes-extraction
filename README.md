c22a2eed98a29623dde38f51eefa82eaab55ec65
montehurd <mhurd@wikimedia.org>
2023-03-02 09:43:23 -0800

    actually move readme bits to readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+background info: 
+	https://ciofecaforensics.com/2017/10/13/theres-gold-in-them-there-blobs/
+	https://github.com/threeplanetssoftware/apple_cloud_notes_parser/tree/master/lib
+	https://stackoverflow.com/questions/34844156/exporting-all-blobs-from-sqlite-to-a-files
+
+to run:
+- grant MacOS terminal the ability to copy the NotesStore.sqlite file: "System Settings > Privacy and Security > Full Disk Access > Terminal"
+		( this can be turned back off after running the script )
+- from the terminal clone this repo:
+		git clone https://github.com/montehurd/macos-notes-extraction.git
+- from terminal change to the directory of the cloned repo:
+		cd ~/macos-notes-extraction
+- from terminal run this file:
+		./extract-notes.sh
+when run notes should appear as files in a new "notes" folder in the directory
+
+limitations:
+- plain text in notes is readable, but attachments such as images aren't decoded 
+		( the github link above looks like it might handle decoding attachments )
--- extract-notes.sh
+++ extract-notes.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# background info: 
-#   https://ciofecaforensics.com/2017/10/13/theres-gold-in-them-there-blobs/
-#   https://github.com/threeplanetssoftware/apple_cloud_notes_parser/tree/master/lib
-#   https://stackoverflow.com/questions/34844156/exporting-all-blobs-from-sqlite-to-a-files
-
-# to run:
-# - grant MacOS terminal the ability to copy the NotesStore.sqlite file: "System Settings > Privacy and Security > Full Disk Access > Terminal"
-#   ( this can be turned back off after running the script )
-# - from the terminal clone this repo:
-#       git clone https://github.com/montehurd/macos-notes-extraction.git
-# - from terminal change to the directory of the cloned repo:
-#       cd ~/macos-notes-extraction
-# - from terminal run this file:
-#      ./extract-notes.sh
-# when run notes should appear as files in a new "notes" folder in the directory
-
-# limitations:
-# - plain text in notes is readable, but attachements such as images aren't decoded
-#   the github link above looks like it handles decoding attachments
-
 # copy the notes sqlite file into our folder
 cp "$HOME/Library/Group Containers/group.com.apple.notes/NoteStore.sqlite" "./"
 # make a folder to hold extracted notes
